bb82429a1c03cf2bbef90a6d6dbd11b65a0ee68a
Thiago Nunes <devthiart@gmail.com>
2024-12-15 12:08:16 -0300

    Queries Methods - Testing Menu component

---
+++ src/componentes/Menu/Menu.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import Menu from './index';
+
+test('Must render link to home page', () => {
+  render(<Menu />);
+  const linkHomePage = screen.getByText('Inicial');
+  expect(linkHomePage).toBeInTheDocument();
+});
+
+test('Must render a list of links', () => {
+  render(<Menu />);
+  const listOfLinks = screen.getAllByRole('link');
+  expect(listOfLinks).toHaveLength(4); // Must have 4 items found.
+});
+
+test('Must not have the bank statement item in the menu', () => {
+  render(<Menu />);
+  const bankStatementLink = screen.queryByText('Extrato');
+  expect(bankStatementLink).not.toBeInTheDocument();
+});
