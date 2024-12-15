0741b3629684a81309d5ce226d311d5350801cfb
Thiago Nunes <devthiart@gmail.com>
2024-12-15 11:42:50 -0300

    Testing a react component

--- src/componentes/Cabecalho/Cabecalho.test.js
+++ src/componentes/Cabecalho/Cabecalho.test.js
@@ -1,4 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import Cabecalho from './index';
+
 test("First Test", () => {
   const number = 10;
   expect(number).toBe(10);
 })
+
+test('The header must contain the username', () => {
+  render(<Cabecalho />);
+  const userName = screen.getByText('Joana Fonseca Gomes');
+  expect(userName).toBeInTheDocument();
+});
